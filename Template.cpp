49295de9b4bf5b54bbf9d6d170003ad19cfea897
Masum Ahmed <31947304+MasumAhmed28@users.noreply.github.com>
2018-07-31 23:52:07 +0600

    template

---
+++ Template.cpp
@@ -0,0 +1,74 @@
+#include<iostream>
+#include<cstdio>
+#include<list>
+#include<string>
+#include<cstring>
+#include<sstream>
+#include<cctype>
+#include<string.h>
+#include<algorithm>
+#include<cmath>
+#include<stack>
+#include<fstream>
+#include<cstdlib>
+#include<vector>
+#include<map>
+#include<set>
+#include<utility>
+#include<iomanip>
+#include<queue>
+#include<deque>
+#include<iterator>
+#include<assert.h>
+#include<bitset>
+#include<climits>
+#include<ctime>
+#include<complex>
+#include<numeric>
+
+using namespace std;
+
+
+#define REP(i,n) for(__typedef(n) i=0;i<n;i++)
+#define FOR(i,a,b) for(__typedef(b) i=(a);i<=(b);i++)
+#define Clear(t) memset((t),0,sizeof(t))
+#define mem(x,y) memset(x,y,sizeof(x))
+#define Forit(i,m) for(__typedef((m).begin() i=(m).begin();i!=(m).end();i++)
+
+#define sz size()
+#define pb push_back
+#define pf push_front
+#define uu first
+#define vv second
+
+#define pii pair<int,int>
+#define VI vector<int>
+#define VS vector<string>
+#define ll long long
+
+#define mem(x,y) memset(x,y,sizeof(x))
+#define uu first
+#define vv second
+#define pii pair<int,int>
+
+#define pi 3.141592653589793238462643383279502884
+#define inf (1<<30)
+#define eps (1e-11)
+
+#define read() freopen("input.txt","r",stdin)
+#define write() freopen("output.txt","w",stdout)
+
+#define debug(x) cerr << __LINE__ <<" "<< #x " = " << x << endl
+
+int fx[]={1,-1,0,0};
+int fy[]={0,0,1,-1};
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    //cout<<"HelloWorld!"<<endl;
+}
+
+
+
